e215cf4599d0bb2563fea6286cfa30545f940953
dlyj518 <dlyj518@gmail.com>
2022-10-13 17:57:51 +0900

    1013

--- test/test.py
+++ test/test.py
@@ -1,43 +1,15 @@
 import sys
-sys.stdin = open('input.txt')
-from collections import deque
-input = sys.stdin.readline
-from pprint import pprint
-import heapq
+from PyQt5.QtWidgets import QApplication, QWidget
 
-dy, dx = [-1, 0, 0, 1], [0, -1, 1, 0]
 
-n = int(input())
-mp = []
-fs = [0]*7
-sl, exp = 2, 0
-for i in range(n):
-    mp.append(list(map(int,input().split())))
-    for j in range(n):
-        if mp[i][j] == 9: sk = deque([(i, j, 0)])
-        elif mp[i][j] != 0: fs[mp[i][j]] += 1
-while 1:
-    if sum(fs[:sl]) == 0: break
-    vs = [[0] * n for _ in range(n)]
-    vs[sk[0][0]][sk[0][1]] = 1
-    mp[sk[0][0]][sk[0][1]] = 0
-    tmp = []
-    st = -1
-    while sk:
-        y, x, c = sk.popleft()
-        if c == st: break
-        if 0 < mp[y][x] < sl:
-            st = c + 1
-            tmp.append((y, x, c))
-        for i in range(4):
-            ny, nx = y+dy[i], x+dx[i]
-            if 0<=ny<n and 0<=nx<n and not vs[ny][nx] and mp[ny][nx] <= sl:
-                vs[ny][nx] = 1; sk.append((ny, nx, c+1))
-    tmp.sort(key = lambda x: (x[0], x[1]))
-    y, x, c = tmp[0]
-    fs[mp[y][x]] -= 1
-    exp += 1
-    mp[y][x] = 9
-    sk = deque([(y, x, c)])
-    if exp == sl: sl += 1; exp = 0
-print(sk[0][2])
+class MyApp(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('My First Application')
+        self.move(300, 300)
+        self.resize(400, 200)
+        self.show()
--- test/test1.py
+++ test/test1.py
@@ -6,11 +6,17 @@ from pprint import pprint
 from math import gcd
 import heapq
 
-ldd = {1 : 2}
-bmd = {99 : 1}
-i = 101
-while i < 102:
-    mn = 1
-    if i in ldd: print(1)
-    if i in bmd: print(2)
-    i += 1
+from itertools import combinations
+t = int(input())
+for tc in range(1, t+1):
+    n, m, k, a, b = map(int, input().split())
+    aa = list(map(int, input().split()))
+    bb = list(map(int, input().split()))
+    tt = list(map(int, input().split()))
+    t = 0
+    at, bt = 0, 0
+    al, bl = [0]*n, [0]*m
+    while 1:
+        at += tt.count(t)
+        for i in range(n):
+            if not al[i]
