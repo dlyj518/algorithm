efe246f6bd57e83d89a204e21ec96ef8759aa9dd
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2022-10-27 17:33:48 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 104 ms, Memory: 32516 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.py
@@ -17,17 +17,15 @@ for i in range(n):
         if mp[i][j] == 'R': ry, rx = i, j
         if mp[i][j] == 'B': by, bx = i, j
         if mp[i][j] == 'O': ot = (i, j)
-rl, bl = [(ry, rx)], [(by, bx)]
-q = deque([[ry, rx, by, bx, rl, bl, -1, 0]])
+q = deque([[ry, rx, by, bx, -1, 0]])
 
 rst = 0
 while q:
-    ry, rx, by, bx, rl, bl, dd, cc = q.popleft()
+    ry, rx, by, bx, dd, cc = q.popleft()
     if cc > 9: continue
     for d in range(4):
         if dd == d: continue
         if dd == (d//2+1)*2-d%2-1: continue
-        nrl = rl[:]; nbl = bl[:]
         nry, nrx, rc = run(ry, rx, d)
         nby, nbx, bc = run(by, bx, d)
         if bc < 0: continue
@@ -35,8 +33,7 @@ while q:
         if nry == nby and nrx == nbx:
             if rc > bc: nry -= dy[d]; nrx -= dx[d]
             else: nby -= dy[d]; nbx -= dx[d]
-        nrl.append((nry, nrx)); nbl.append((nby, nbx))
-        q.append([nry, nrx, nby, nbx, nrl, nbl, d, cc+1])
+        q.append([nry, nrx, nby, nbx, d, cc+1])
     if rst: break
 if rst: print(rst)
 else: print(-1)
