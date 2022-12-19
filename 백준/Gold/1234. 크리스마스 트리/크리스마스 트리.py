227976d942a5f21a6934ef62f288c343e7b11408
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2022-12-20 00:39:25 +0900

    [Gold II] Title: 크리스마스 트리, Time: 7108 ms, Memory: 147040 KB -BaekjoonHub

---
+++ 백준/Gold/1234. 크리스마스 트리/크리스마스 트리.py
@@ -0,0 +1,18 @@
+from math import factorial as f
+
+def dfs(m, rr, gg, bb):
+    if m > n: return 1
+    c = 0
+    for i in range(min(n, rr)+1):
+        for j in range(min(n-i, gg)+1):
+            k = m-i-j
+            if k > bb or k < 0: continue
+            tt = []
+            for t in [i, j, k]:
+                if t: tt.append(t)
+            if max(tt) != min(tt): continue
+            c += dfs(m+1, rr-i, gg-j, bb-k) * f(i+j+k)//(f(i)*f(j)*f(k))
+    return c
+
+n, r, g, b = map(int, input().split())
+print(dfs(1, r, g, b))
