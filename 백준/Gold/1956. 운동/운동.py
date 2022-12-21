6a9759e80a8c647ff910b749e98b2d6363731d40
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2022-12-22 01:27:06 +0900

    [Gold IV] Title: 운동, Time: 800 ms, Memory: 86444 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,10 +1,21 @@
+from heapq import *
+import sys
+input = sys.stdin.readline
+
 v, e = map(int, input().split())
-dr = [[1e8]*v for _ in range(v)]
+dp = [[1e8]*(v+1) for _ in range(v+1)]
+rd = {i: [] for i in range(v+1)}
+q = []
 for _ in range(e):
     a, b, c = map(int, input().split())
-    dr[a-1][b-1] = c
-for k in range(v):
-    for i in range(v):
-        for j in range(v): dr[i][j] = min(dr[i][j], dr[i][k] + dr[k][j])
-x = min([dr[i][i] for i in range(v)])
-print(x) if x < 1e8 else print(-1)
+    dp[a][b] = c
+    rd[a].append((c, b))
+    heappush(q, (c, a, b))
+while q:
+    c, a, b = heappop(q)
+    if a == b: print(c); break
+    if c > dp[a-1][b]: continue
+    for k, r in rd[b]:
+        if c + k < dp[a][r]:
+            dp[a][r] = c + k; heappush(q, (c+k, a, r))
+else: print(-1)
