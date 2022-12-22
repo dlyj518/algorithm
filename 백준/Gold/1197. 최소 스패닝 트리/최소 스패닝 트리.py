182b7009932c145122bac89a00e82df8d9b8be8f
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2022-12-22 18:03:08 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 352 ms, Memory: 55852 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+def fnd(x):
+    if pr[x] != x: return fnd(pr[x])
+    return x
+
+v, e = map(int, input().split())
+pr = list(range(v+1))
+ls = []
+for _ in range(e):
+    ls.append(list(map(int, input().split())))
+ls.sort(key=lambda x: x[2])
+rst = 0
+for a,b,c in ls:
+    a, b = map(fnd, (a, b))
+    if a != b:
+        if a < b: pr[b] = a
+        else: pr[a] = b
+        rst += c
+print(rst)
