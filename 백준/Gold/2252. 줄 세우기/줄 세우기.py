522b8d5023eacdfb5a57ddb63be7648302c5d2c0
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2022-12-24 18:32:44 +0900

    [Gold III] Title: 줄 세우기, Time: 252 ms, Memory: 122220 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -2,19 +2,18 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-pr = [[] for _ in range(n + 1)]
+pr = [0]*(n + 1)
 ch = [[] for _ in range(n + 1)]
 for _ in range(m):
     a, b = map(int, input().split())
-    pr[b].append(a); ch[a].append(b)
+    pr[b] += 1; ch[a].append(b)
 st = []; rst = []
-while 1:
-    for i in range(1, n+1):
-        if not pr[i]: st.append(i)
-    if not st: break
-    while st:
-        j = st.pop()
-        for k in ch[j]: pr[k].remove(j)
-        rst.append(j)
-        pr[j] = [-1]
+for i in range(1, n+1):
+    if not pr[i]: st.append(i)
+while st:
+    j = st.pop()
+    for k in ch[j]:
+        pr[k] -= 1
+        if not pr[k]: st.append(k)
+    rst.append(j)
 print(*rst)
