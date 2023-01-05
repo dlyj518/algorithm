61b6811af0c06aedbf6d9a12673ba84ddfc2b07f
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2023-01-05 16:26:21 +0900

    [Gold IV] Title: 문자열 폭발, Time: 412 ms, Memory: 48484 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -4,8 +4,7 @@ n = len(p)
 st = []
 for a in x:
     st.append(a)
-    if len(st) >= n and st[-n:] == p:
+    if st[-n:] == p:
         for i in range(n): st.pop()
 if len(st) == 0: print('FRULA')
-else:
-    for i in st: print(i, end='')
+else: print(''.join(st))
