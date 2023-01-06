ada654f3908a1309ea3a6f410e936aa3a23b665d
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2023-01-06 13:11:36 +0900

    [Gold II] Title: 배드민턴 복식 팀 만들기, Time: 232 ms, Memory: 56660 KB -BaekjoonHub

---
+++ 백준/Gold/23762. 배드민턴 복식 팀 만들기/배드민턴 복식 팀 만들기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+st = sorted(enumerate(list(map(int, input().split()))), key=lambda x: x[1])
+idx = []; pl = []
+for i, x in st: idx.append(i); pl.append(x)
+sm = [1e9] * n
+for i in range(3, n): sm[i] = pl[i] - pl[i - 3]
+ls = [[0], [0, 1], [0, 1, 2], []]
+dp = [0] * n
+dp[3] = sm[3]
+for i in range(4, n):
+    if i % 4 == 3: dp[i] = dp[i - 4] + sm[i]; ls.append([])
+    else:
+        if dp[i - 1] < sm[i] + dp[i - 4]:
+            dp[i] = dp[i - 1]; ls.append(ls[i-1] + [i])
+        else: dp[i] = sm[i] + dp[i - 4]; ls.append(ls[i - 4]) 
+print(dp[-1])
+for a in ls[-1]: print(idx[a])
