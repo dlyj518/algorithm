6187515a5cbc12b3cd858c0129e9acd3a8c0da7d
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2023-01-17 11:23:12 +0900

    [Bronze II] Title: 할리갈리, Time: 4184 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -0,0 +1,8 @@
+n = int(input())
+fr = {'STRAWBERRY': 0, 'BANANA': 0, 'LIME': 0, 'PLUM': 0}
+for _ in range(n):
+    f, i = input().split()
+    fr[f] += int(i)
+for f in fr:
+    if fr[f] == 5: print('YES'); break
+else: print('NO')
