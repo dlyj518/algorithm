db6f8a274b4a2617b8f367eda2c39434ceb41a62
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2024-01-04 05:12:22 +0900

    [Silver I] Title: 나무 탈출, Time: 1660 ms, Memory: 146536 KB -BaekjoonHub

--- 백준/Silver/15900. 나무 탈출/나무 탈출.py
+++ 백준/Silver/15900. 나무 탈출/나무 탈출.py
@@ -2,14 +2,6 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(600000)
 
-def dfs(x, a):
-    global r
-    ch[x] = 0
-    c = 1
-    for i in tr[x]:
-        if ch[i]: dfs(i, a + 1); c = 0
-    if c: r += a
-
 n = int(input())
 tr = [[] for _ in range(n + 1)]
 ch = [1] * (n + 1)
@@ -17,5 +9,12 @@ for _ in range(n - 1):
     a, b = map(int, input().split())
     tr[a].append(b); tr[b].append(a)
 r = 0
-dfs(1, 0)
+st = [(1, 0)]
+while st:
+    x, y = st.pop(-1)
+    ch[x] = 0
+    c = 1
+    for i in tr[x]:
+        if ch[i]: st.append((i, y + 1)); c = 0
+    if c: r += y
 print('Yes') if r % 2 else print('No')
