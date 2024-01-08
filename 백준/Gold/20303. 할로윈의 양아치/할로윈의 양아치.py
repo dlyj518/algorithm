7433b967ba44d174f933d69aece84a32070cda02
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2024-01-09 03:11:23 +0900

    [Gold III] Title: 할로윈의 양아치, Time: 624 ms, Memory: 1550904 KB -BaekjoonHub

--- 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
+++ 백준/Gold/20303. 할로윈의 양아치/할로윈의 양아치.py
@@ -1,5 +1,13 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def dfs(x):
+    vs[x] = 0
+    w, c = 1, cn[x]
+    for i in tr[x]:
+        if vs[i]: [a, b] = dfs(i); w += a; c += b
+    return [w, c]
 
 n, m, k = map(int, input().split())
 cn = [0] + list(map(int, input().split()))
@@ -13,12 +21,6 @@ vs = [1] * (n + 1)
 dp = [0] * k
 for i in range(1, n + 1):
     if vs[i]:
-        w = c = vs[i] = 0
-        st = [i]
-        while st:
-            x = st.pop()
-            w += 1; c += cn[x]
-            for a in tr[x]:
-                if vs[a]: vs[a] = 0; st.append(a)
-        for i in range(k - 1, w - 1, -1): dp[i] = max(dp[i], dp[i - w] + c)
+        a, b = dfs(i)
+        for i in range(k - 1, a - 1, -1): dp[i] = max(dp[i], dp[i - a] + b)
 print(dp[-1])
