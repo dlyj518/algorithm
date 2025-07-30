6ed2a79f7f1a212a56b3f7cdfda9e647791bf844
dlyj518 <109326258+dlyj518@users.noreply.github.com>
2025-07-30 15:01:41 +0900

    [Silver I] Title: 1, 2, 3 더하기 7, Time: 608 ms, Memory: 50460 KB -BaekjoonHub

---
+++ 백준/Silver/15992. 1， 2， 3 더하기 7/1， 2， 3 더하기 7.py
@@ -0,0 +1,11 @@
+dp = [[0] * 1001 for _ in range(1001)]
+dp[1][1] = dp[2][1] = dp[2][2] = dp[3][1] = dp[3][3] = 1
+dp[3][2] = 2
+
+for i in range(4, 1001):
+    for j in range(2, 1001):
+        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 2][j - 1] + dp[i - 3][j - 1]) % int(1e9 + 9)
+
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    print(dp[n][m])
